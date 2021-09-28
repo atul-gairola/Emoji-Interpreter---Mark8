fdbd2e18a76dad444e85c246c6e95ef58c7f41ba
atul-gairola <atulgairola08@gmail.com>
2021-09-28 16:45:09 +0530

    style: basic styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
+import { emojisData } from "./emojiData";
+import "./App.css";
+
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <header>
         <h1>Know your emojies</h1>
       </header>
       <main>
-        <input
-          type="text"
-          placeholder="Paste your emoji here to know it's name"
-        />
-        <div></div>
+        <form className="inputForm">
+          <input
+            type="text"
+            placeholder="Paste your emoji here to know it's name"
+          />
+          <button>Clear</button>
+        </form>
+        <div className="emojiContainer">
+          {Object.keys(emojisData)
+            .slice(0, 15)
+            .map((key) => (
+              <button className="emojiButton" id={key}>
+                {key}
+              </button>
+            ))}
+        </div>
       </main>
-      <footer>
+      <footer className="footer">
         <p>
           Made with ❤️ by{" "}
           <a href="https://www.atulgairola.tech/">Atul Gairola </a>
